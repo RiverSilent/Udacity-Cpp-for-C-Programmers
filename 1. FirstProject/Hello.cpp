a69291c2cf54689836ca85725789e95e9beeb3d4
unknown <prateergamer@gmail.com>
2018-04-14 12:11:55 +0530

    Code to print Hello World

---
+++ 1. FirstProject/Hello.cpp
@@ -0,0 +1,18 @@
+/*
+ * Hello.cpp
+ *
+ *  Created on: 14-Apr-2018
+ *      Author: River
+ */
+
+#include "hello.hpp"
+
+int main(){
+
+	cout << "Hello World!" << endl;
+
+	return 0;
+}
+
+
+
---
+++ 1. FirstProject/hello.hpp
@@ -0,0 +1,16 @@
+/*
+ * hello.hpp
+ *
+ *  Created on: 14-Apr-2018
+ *      Author: River
+ */
+
+#ifndef HELLO_HPP_
+#define HELLO_HPP_
+
+#include <iostream>
+using namespace std;
+
+
+
+#endif /* HELLO_HPP_ */
