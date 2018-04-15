127e812bc79933ed6f0b208a13b28e93f7223f78
unknown <prateergamer@gmail.com>
2018-04-15 19:00:38 +0530

    Code demonstrating the use of getline method for user inputs.

---
+++ 7. UserInputs/src/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 15-Apr-2018
+ *      Author: River
+ */
+
+#include "main.h"
+
+int main(){
+
+	string nickname;
+
+	cout << "Hey! What's your nickname? " <<endl;
+	getline(cin,nickname);
+	cout << "Hello " << nickname << "!!!" << endl;
+
+}
+
+
